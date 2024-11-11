1c9495c9977ecd02907e4889a88437c569b7a84c
marioUnir <mario.rio193@comunidadunir.net>
2024-11-11 20:05:54 +0000

    cambios

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ print("hello World")
 
 print("1")
 
+import pandas as pd
 from codigos.EDA import EDAProcessor
+
+# Load the dataset
+data = pd.read_csv('data/UCI_Credit_Card.csv')
+print(data.head())
---
+++ replit.nix
@@ -0,0 +1,6 @@
+{pkgs}: {
+  deps = [
+    pkgs.pop
+    pkgs.glibcLocales
+  ];
+}
